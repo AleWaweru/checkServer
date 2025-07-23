4cb58b04e55831f9d69d32e3404c226ce3590083
AleWaweru <ngashalex8@gmail.com>
2025-07-23 15:35:13 +0300

    all Users

--- controllers/authController.js
+++ controllers/authController.js
@@ -233,3 +233,15 @@ export const resetPassword = async (req, res) => {
     res.status(500).json({ message: "Error resetting password", error: err.message });
   }
 };
+
+export const allUsers = async (req, res) => {
+  try {
+    const users = await User.find().select("-password -resetPasswordToken -resetPasswordExpires -__v");
+
+    res.status(200).json({ users });
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res.status(500).json({ message: "Failed to fetch users", error: error.message });
+  }
+};
+
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,5 +1,7 @@
 import express from "express";
-import { register, login, verifyEmail, signUpGoogle, forgotPasswordRequestController} from "../controllers/authController.js";
+import { register, login, verifyEmail, signUpGoogle, forgotPasswordRequestController, allUsers} from "../controllers/authController.js";
+import { verifyToken } from "../middleware/jwt/verifyToken.js";
+import { isAdmin } from "../middleware/roles/adminRoles.js";
 
 const router = express.Router();
 
@@ -8,6 +10,7 @@ router.post("/login", login);
 router.get("/verify-email", verifyEmail);
 router.post("/googleAuth", signUpGoogle);
 router.post("/forgot-password", forgotPasswordRequestController);
+router.get("/allUsers", verifyToken, isAdmin, allUsers);
 
 
 export default router;
